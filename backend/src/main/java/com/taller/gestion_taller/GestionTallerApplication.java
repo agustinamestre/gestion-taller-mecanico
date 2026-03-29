0be15e37ab9347297b8a1caefeafea5ecb0af128
amestre <agustinamestre@macro.com.ar>
2026-03-29 14:26:01 -0300

    GESTION-CLIENTE MapStruct

--- backend/src/main/java/com/taller/gestion_taller/GestionTallerApplication.java
+++ backend/src/main/java/com/taller/gestion_taller/GestionTallerApplication.java
@@ -1,13 +1,20 @@
 package com.taller.gestion_taller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class GestionTallerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionTallerApplication.class, args);
+        SpringApplication.run(GestionTallerApplication.class, args);
+
+        log.info("  ___   _  __  _ ");
+        log.info(" / _ \\ | |/ / | |");
+        log.info("| (_) || ' <  |_|");
+        log.info(" \\___/ |_|\\_\\ (_)");
 	}
 
 }
