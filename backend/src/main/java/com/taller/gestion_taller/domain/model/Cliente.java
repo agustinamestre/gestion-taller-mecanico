009eb1f586670e613f6434cffd63fe7be70151cf
amestre <agustinamestre@macro.com.ar>
2026-03-29 14:25:10 -0300

    GESTION-CLIENTE dominio cliente

--- backend/src/main/java/com/taller/gestion_taller/domain/model/Cliente.java
+++ backend/src/main/java/com/taller/gestion_taller/domain/model/Cliente.java
@@ -1,4 +1,28 @@
 package com.taller.gestion_taller.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder(toBuilder = true)
+@AllArgsConstructor
 public class Cliente {
+
+    private Long id;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String direccion;
+    @Builder.Default
+    private boolean activo = true;
+    @Builder.Default
+    private LocalDate fechaCreacion = LocalDate.now();
+    @Builder.Default
+    private LocalDate fechaModificacion = LocalDate.now();
+
 }
