47c23a023b8736899c8561ebbe16fc89096b4e2f
amestre <agustinamestre@macro.com.ar>
2026-04-06 21:49:12 -0300

    Listar Marcas

--- backend/src/main/java/com/taller/gestion_taller/application/usecases/marca/ListarMarcas.java
+++
@@ -1,9 +0,0 @@
-package com.taller.gestion_taller.application.usecases.marca;
-
-import com.taller.gestion_taller.domain.model.Marca;
-
-import java.util.List;
-
-public interface ListarMarcas {
-    List<Marca> listarMarcas();
-}
