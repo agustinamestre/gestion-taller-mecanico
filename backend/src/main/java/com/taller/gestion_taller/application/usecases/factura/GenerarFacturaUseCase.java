514cf17e1e960596923fcb8916e0cad64a519c81
amestre <agustinamestre@macro.com.ar>
2026-09-24 04:05:09 -0300

    generar factura use case

--- backend/src/main/java/com/taller/gestion_taller/application/usecases/factura/GenerarFacturaUseCase.java
+++ backend/src/main/java/com/taller/gestion_taller/application/usecases/factura/GenerarFacturaUseCase.java
@@ -5,6 +5,8 @@ import com.taller.gestion_taller.domain.exception.BusinessErrors;
 import com.taller.gestion_taller.domain.exception.NotFoundException;
 import com.taller.gestion_taller.domain.model.Factura;
 import com.taller.gestion_taller.domain.model.OrdenTrabajo;
+import com.taller.gestion_taller.domain.model.TipoComprobante;
+import com.taller.gestion_taller.domain.repositories.ContadorFacturaRepository;
 import com.taller.gestion_taller.domain.repositories.FacturaRepository;
 import com.taller.gestion_taller.domain.repositories.OrdenTrabajoRepository;
 import com.taller.gestion_taller.domain.service.FacturaValidator;
@@ -14,9 +16,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GenerarFacturaUseCase implements GenerarFactura {
 
+    private static final String FORMATO_NUMERO_FACTURA = "F%08d";
+
+    // Facturación asume taller monotributista -> siempre Factura C
+    private static final TipoComprobante TIPO_COMPROBANTE = TipoComprobante.C;
+
     private final FacturaRepository facturaRepository;
     private final FacturaValidator facturaValidator;
     private final OrdenTrabajoRepository ordenTrabajoRepository;
+    private final ContadorFacturaRepository contadorFacturaRepository;
 
     @Override
     public Factura generarFactura(GenerarFacturaCommand command) {
@@ -26,11 +34,14 @@ public class GenerarFacturaUseCase implements GenerarFactura {
 
         facturaValidator.validarOrdenParaFacturacion(orden);
 
-        Factura factura = Factura.crearNueva(orden, command.getFormaPago());
+        String numeroFactura = String.format(FORMATO_NUMERO_FACTURA, contadorFacturaRepository.siguienteNumero());
+        Factura factura = Factura.crearNueva(orden, command.getFormaPago(), numeroFactura, TIPO_COMPROBANTE);
         Factura guardada = facturaRepository.save(factura);
 
-        String numeroFactura = String.format("F%08d", guardada.getId());
-        return facturaRepository.actualizarNumeroFactura(guardada.getId(), numeroFactura);
+        orden.marcarComoFacturada();
+        ordenTrabajoRepository.save(orden);
+
+        return guardada;
     }
 
 }
